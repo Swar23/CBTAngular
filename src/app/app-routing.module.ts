c640dc1c74849b2494c9008daa8a090f925fdadb
Swarup <swarupc@rocketmail.com>
2021-02-27 13:23:35 +0530

    Added new components and set the routing paths

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { ErrorComponent } from './error/error.component';
+import { LoginComponent } from './login/login.component';
+
+const routes: Routes = [{ path: 'login', component: LoginComponent },
+{ path: '', redirectTo: 'login', pathMatch: 'full' },
+{ path: '**', component: ErrorComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
