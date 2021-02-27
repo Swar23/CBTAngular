66dd19a46e875a09685eb4ff8f8b8894691cfa65
Swarup <swarupc@rocketmail.com>
2021-02-27 13:58:23 +0530

    Login Screen Display work done

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -7,9 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  email: string;
+  password: string;
+  data;
+  public error: string;
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onSubmit() {
+
+
+  }
 }
